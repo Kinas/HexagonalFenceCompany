6dda4b198f2de0036d8bc8df446ca64648fdd24e
Angelika Kinas <kinas@stud.uni-heidelberg.de>
2018-05-30 19:49:41 +0200

    fixes #1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css';
 //import the table-strucutre from index
 import App from './index.js'
-
+import companies from './companies';
 
 
 class GitForm extends React.Component {
@@ -10,64 +10,31 @@ class GitForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      repoName: '',
-      ownerName: '',
-      query: '',
       results: []
-  };
-    this.handleRepoChange = this.handleRepoChange.bind(this);
-    this.handleOwnerChange = this.handleOwnerChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  };  
     this.getInfo = this.getInfo.bind(this);
   }
-  //get data from gtiHub API
+  //get data from data file(s)
   getInfo() {
     var data=[];
-    fetch('https://api.github.com/repos/'+ this.state.query[1] + '/'+ this.state.query[0] +'/issues\?state\=all')
-    .then((response) => response.json())
-    .then((responseJson) => {
-      responseJson.forEach(id => {
-        data.push({
-          number: id.number,
-            title: id.title,
-            date: id.created_at
+    console.log(companies, companies.comp001.title)
+    for (const key of Object.keys(companies)) {
+          data.push({
+          name: companies[key].title,
+          id: key
         });
-      })
-    })
-    .then(() => {
-      //after data is fetched change the state so React can react and update the table
-      this.setState({results: data})
-    })
-    .catch((error) => {
-      console.error(error);
-    });  
-  }
-  //input handler
-  handleRepoChange(event) {
-    this.setState({repoName: event.target.value});
-  }
-   handleOwnerChange(event) {
-    this.setState({ownerName: event.target.value});
-  }
-  //submit handler
-  handleSubmit(event) {
-    //after submitting repo and owner name update the state and fetch the data
-    this.setState({
-      query: [this.state.repoName, this.state.ownerName]
-    }, () => {
-            this.getInfo()
-    });
-    event.preventDefault();
+    }
+
+    console.log(data)
+    this.setState({results: data})
   }
   //render the form
   render() {
     return (
       <div className="React-form">
-        <div className="topnav">
-            <input type="text" name="repoName"  placeholder="Repository" onChange={this.handleRepoChange} />
-            <input type="text" name="ownerName" placeholder="Owner" onChange={this.handleOwnerChange} />
-            <img src="search.png" onClick={this.handleSubmit} alt="submit"/>
-        </div>
+
+      <img src="search.png" onClick={this.getInfo} alt="submit"/>
+
       <span id="table-container">
           <App changeSets={this.state.results} />
       </span>
--- src/index.js
+++ src/index.js
@@ -1,71 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//getting the data for setting up the table
-import GitForm from './App.js'
-
-class Heading extends React.Component {
-  render() {
-    return <th>{this.props.heading}</th>;
-  }
-}
-
-class Headings extends React.Component {
-  render() {
-    var headings = this.props.headings.map(function(name, i) {
-      return <Heading heading = {name} key={i}/>;
+import Reacttable from './table'
+import companies from './companies';
+import categories from './categories.js';
+import compcat  from './category_per_company.js';
+//get data from data file(s)
+/*
+@param dataOject: JSON Object
+*/
+function getInfo(dataObject){
+  var data=[];
+  for (const key of Object.keys(dataObject)) {
+      data.push({
+      name: dataObject[key].title,
+      id: key
     });
-   return <thead><tr>{headings}</tr></thead>;
   }
+  return data
 }
+var companiesData = getInfo(companies);
+var categoriesData =getInfo(categories);
+var compcatData = getInfo(compcat);
 
-class Row extends React.Component {
-  render() {
-    return (<tr>
-             <td>{this.props.changeSet.number}</td>
-             <td>{this.props.changeSet.title}</td>
-             <td>{this.props.changeSet.date}</td>
-           </tr>);
-  }
-}
+console.log(categoriesData)
+console.log(compcatData)
 
-class Rows extends React.Component {
-  render() {
-    for (var i=0; i<this.props.changeSets.length; i++){
-    }
-    var rows = this.props.changeSets.map(function(changeSet, i) {
-      return(<Row changeSet = {changeSet} key={i}/>);
-    });
-    return <tbody>{rows}</tbody>;
-  }
-
-}
-
-//main class: App
-// creates a table with the given data
-class App extends React.Component { 
+class Container extends React.Component {
+  //constructs the state of the class
   constructor(props) {
-      super(props);
-      this.state = {
-        headings: ['Number', 'Title', 'Date of Creation']
-    };
+    super(props);
+    this.state = {
+        data: companiesData
+    };  
   }
-        
-    render() {
-    return <table className = 'table'>
-             <Headings headings = {this.state.headings} />
-             <Rows changeSets = {this.props.changeSets} />
-           </table>;
-    }
 
+//render the container with the table
+  render() {
+    return (
+      <div className="React-table">
+          <Reacttable changeSets={this.state.data} />
+      </div>
+    );
+  }
 }
- 
-export default App;
 
 
 // =================================
 
-ReactDOM.render(<GitForm />, document.getElementById('body'));
+ReactDOM.render(<Container />, document.getElementById('body'));
 
 
 
